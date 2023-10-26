596f23e2c19cc5b7f0dddf1bf95907ea267360f8
Ricardo Ruelas <ricardo@pc-rrv.fi.unam>
2023-10-25 20:43:48 -0600

    New: Created user-profile microfrontend.

---
+++ user-profile/app/page.jsx
@@ -0,0 +1,14 @@
+import Navbar from '@/components/navbar';
+import { Grid } from '@mui/material';
+
+export default function Home() {
+  return (
+    <Grid 
+      container
+      justifyContent='center'
+      alignItems='center'
+      style={{ height: '100vh', marginTop: '-24px' }}
+    >
+    </Grid>
+  );
+}
