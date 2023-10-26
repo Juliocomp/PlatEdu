60d386d12f655d1214898dc9891d3a159935e9b7
Ricardo Ruelas <ricardo@pc-rrv.fi.unam>
2023-10-25 22:27:56 -0600

    Change: page.jsx is now home.jsx and is placed in pages directory.

---
+++ user-profile/pages/home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Grid } from '@mui/material';
+import Navbar from '@/components/navbar';
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar/>
+      <Grid 
+        container
+        justifyContent='center'
+        alignItems='center'
+        style={{ height: '100vh', marginTop: '-24px' }}
+      >
+        HOME
+      </Grid>
+    </div>
+  )
+}
