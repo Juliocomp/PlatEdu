48fba3b2ec8a74bf0d8d975717afabe08103f041
Ricardo Ruelas <ricardo@pc-rrv.fi.unam>
2023-10-30 22:14:57 -0600

    Feature: Trying to expose profile page. NOT WORKING.

--- main/user-profile/next.config.js
+++ main/user-profile/next.config.js
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
+const { NextFederationPlugin } = require('@module-federation/nextjs-mf');
+
+const deps = require('./package.json').dependencies;
+const nextConfig = {
+  reactStrictMode: true,
+  webpack(config, options) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "UserProfile",
+        filename: "static/chunks/remoteEntry.js",
+        remotes: {
+        },
+        exposes: {
+          './profile': './pages/profile.jsx'
+        },
+        shared: {
+          ...deps,
+          react: { singleton: true, eager: true, requiredVersion: deps.react },
+          'react-dom': {
+            singleton: true,
+            eager: true,
+            requiredVersion: deps['react-dom'],
+          }, 
+        },
+      })
+    );
+    return config;
+  }
+}
+   
 module.exports = nextConfig
