c76979f5222f420fe03bd2fab9ee71021e868e03
Ricardo Ruelas <ricardo@pc-rrv.fi.unam>
2023-11-06 16:38:46 -0600

    Feature: Session info changes in sessionStorage and LOG MESSAGES added to functionalities.

--- components/profile-settings.jsx
+++ components/profile-settings.jsx
@@ -40,18 +40,86 @@ export default function ProfileSettings() {
 
   useEffect(() => {
     if (status === "authenticated" && session) {
-      setUsernameValue(session.user.name);
-      setEmailValue(session.user.email);
-      setPasswordValue(session.user.passwordValue);
-      setSelectedImage(session.user.image);
+      console.log('INFO: Loading user info from session.');
+      const customUsername = sessionStorage.getItem('customUsername');
+      const customEmail = sessionStorage.getItem('customEmail');
+      const customPassword = sessionStorage.getItem('customPassword');
+      const customImage = sessionStorage.getItem('customImage');
+
+      if (customUsername) {
+        setUsernameValue(customUsername);
+        console.log('INFO: Username value loaded: ' + customUsername);
+      }
+      else{
+        setUsernameValue(session.user.name);
+        console.log('INFO: Username value loaded: ' + session.user.name);
+      }
+
+      if (customEmail) {
+        setEmailValue(customEmail);
+        console.log('INFO: Email value loaded: ' + customEmail);
+      }
+      else{
+        setEmailValue(session.user.email);
+        console.log('INFO: Email value loaded: ' + session.user.email);
+      }
+
+      if (customPassword){
+        setPasswordValue(customPassword);
+        console.log('INFO: Password value loaded.');
+      }
+      else{
+        setPasswordValue(session.user.passwordValue);
+        console.log('INFO: Password value loaded.');
+      }
+
+      if (customImage){
+        setSelectedImage(customImage);
+        console.log('INFO: Image value loaded: ' + customImage);
+      }
+      else{
+        setSelectedImage(session.user.image);
+        console.log('INFO: Image value loaded: ' + session.user.image);
+      }
+      
+      console.log('SUCCESS: Info user loaded correctly.');
     }
   }, [status, session]);
 
   const handleImageChange = (e) => {
+    console.log('INFO: Handling image change.');
     const file = e.target.files[0];
     if (file) {
+      console.log('INFO: File exists and loaded correctly.');
       const imageUrl = URL.createObjectURL(file);
       setSelectedImage(imageUrl);
+      console.log('SUCCESS: Image changed.');
+    }
+    console.log("WARNING: Can't load image. File not exits or is corrupted.");
+  };
+
+  const handleSave = () => {
+    console.log('INFO: Save button pressed.');
+    if (status === "authenticated" && session) {
+      console.log('INFO: Enter auth condition.');
+      
+      sessionStorage.setItem('customUsername', usernameValue);
+      console.log('INFO: Custom user name saved. Value: ' + usernameValue);
+
+      sessionStorage.setItem('customEmail', emailValue);
+      console.log('INFO: Custom email saved. Value: ' + usernameValue);
+
+      sessionStorage.setItem('customPassword', passwordValue);
+      console.log('INFO: Custom password saved. Value: ' + usernameValue);
+
+      sessionStorage.setItem('customImage', selectedImage);
+      console.log('INFO: Custom image saved. Value: ' + selectedImage);
+
+      console.log('INFO: New values in session: ', session);
+      console.log('SUCCESS: Save button event ended.');
+    }
+    else{
+      console.log('Error: Save button is not working.');
     }
   };
 
@@ -129,6 +197,7 @@ export default function ProfileSettings() {
               component="span"
               style={{ backgroundColor: 'green', color: 'white', width: "200px", height: "40px", marginLeft:'10px', marginBottom: '25px'}}
               fullWidth
+              onClick={handleSave}
             >
               Save
             </StyledButton>
