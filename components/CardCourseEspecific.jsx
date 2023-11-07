e7dc52f5952431c1bd2671caa27238e5dc28e0e6
elviaBautista <elvia.bautista@ingenieria.unam.edu>
2023-11-07 01:45:39 -0600

    modificacion del contenido de las tarjetas para novedades

--- components/CardCourseEspecific.jsx
+++ components/CardCourseEspecific.jsx
@@ -12,37 +12,16 @@ import Data from '@/app/data.json';
 function Capp() {
   return (
       <Container maxWidth="lg" style={{marginTop:"50 px", background: "black"}}> 
-            <h1>hola</h1>
       <Grid container spacing={10} style={{marginTop:'20px'}}>
         {Data.map((result,index)=>(
             <Grid item sx={{ width: '100%', display: 'flex', flexDirection: 'column' }} key={index}>
                           <Card >
-<<<<<<< HEAD
-=======
-      <CardMedia
-<<<<<<< HEAD
-        sx={{ height: 70 }}
-        image="/static/images/cards/contemplative-reptile.jpg"
-=======
-        sx={{ height: 140 }}
-        image={result.img}
->>>>>>> db9a144f089fde92b949a5a3f7d2301196c4cf75
-        title="green iguana"
-      />
->>>>>>> 4a8d0720394addfda745487920cf5bdbb8ffdcb1
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-<<<<<<< HEAD
-          TAREA 1 
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Para esta primera tarea se desea investigar sobre el "Boom Latinoamerocano", enlistar los principales autores y su obras.
-=======
+                          <CardContent>
+                          <Typography gutterBottom variant="h5" component="div">
             {result.title}
         </Typography>
         <Typography variant="body2" color="text.secondary">
             {result.des}
->>>>>>> db9a144f089fde92b949a5a3f7d2301196c4cf75
         </Typography>
       </CardContent>
       <CardActions>
