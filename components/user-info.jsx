32f1595dc941b138675f856cc415ebda90742856
alexander21TORRES <54383745+alexander21TORRES@users.noreply.github.com>
2023-11-06 23:49:57 -0600

    feature: Home
    creating a home page for authenticated users

--- components/user-info.jsx
+++ components/user-info.jsx
@@ -3,12 +3,82 @@
 
 import LogForm from '@/components/logform'
 import { useSession } from 'next-auth/react';
+import { Grid } from '@mui/material';
+import SchoolIcon from '@mui/icons-material/School';
+import Typography from '@mui/material/Typography';
 
 
 export default function UserInfo() {
     const { status, data: session } = useSession();
 
-    
+    console.log('INFO: Status value: ' + status);
+
+    if(status=== 'authenticated')
+    {
+        console.log('INFO: Home page');
+        return (
+            <div>
+              
+              <Grid 
+                container
+                justifyContent='center'
+                alignItems='center'
+                style={{ height: '25vh', marginTop: '-24px', marginBottom:'0px'}}
+              >
+                <SchoolIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+              <Typography
+                variant="h1"
+                noWrap
+                component="a"
+                href="/"
+                sx={{
+                  mr: 2,
+                  display: { xs: 'none', md: 'flex' },
+                  fontFamily: 'monospace',
+                  fontWeight: 700,
+                  letterSpacing: '.3rem',
+                  color: 'inherit',
+                  textDecoration: 'none',
+                }}
+              >
+                EduPlat
+                
+                
+              </Typography>
+              
+                
+              </Grid>
+              <Typography
+                variant="h3"
+                noWrap
+                component="a"
+                href="/"
+                sx={{
+                  mr: 2,
+                  display: { xs: 'none', md: 'flex' },
+                  fontFamily: 'monospace',
+                  fontWeight: 700,
+                  letterSpacing: '.3rem',
+                  color: 'inherit',
+                  textDecoration: 'none',
+                }}
+              >
+                
+                
+                The Educational Plataform of the future
+              </Typography>
+               <br />
+               
+              
+               
+              
+            </div>
+            
+          )
+    }
+    else{
+        console.log('INFO: Login Form Redirect');
         return <LogForm/>;
+      }  
     
 }
