2980b8166169142b7af8e7915cad721f4f71fb33
Juliocomp <jules200102@gmail.com>
2023-12-06 20:39:07 -0600

    feature_signin_buttons_begin_popups

--- components/logform.jsx
+++ components/logform.jsx
@@ -6,19 +6,35 @@ import { InputAdornment } from '@mui/material';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import { signIn } from 'next-auth/react';
 import SignInBtn from './signin-btn';
+import Popup from './PopupForgot';
+import Popup from './PopupRegister';
+
 
 
 const LogForm = () => {
-  const buttonStyle = {
-    fontSize: '12px',
-    padding: '5px'
-  };
-  
+/*
+  const [showPopup, setShowPopup] = useState(false);
+  const [dataFromPopup, setDataFromPopup] = useState('');
 
+  const handleOpenPopup = () => {
+    setShowPopup(true);
+  };
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
+
+  const handleReceiveData = (data) => {
+    setDataFromPopup(data);
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
-    
   };
+*/
+
+
+
+
 
   return (
     <form onSubmit={handleSubmit}>
@@ -86,6 +102,30 @@ const LogForm = () => {
       
       <br />
       <br />
+
+      <Button variant="contained"
+        color="primary" 
+        type="submit" 
+        style={{
+        borderRadius: 35,
+        backgroundColor: "#212121",
+        paddingTop: "7px",
+        fontSize: "10px",
+        width:"100px",
+        height:"30px",
+       
+        }}
+      sx={{
+        borderRadius: '28px',
+        padding: '8px 16px',
+        boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.2)',
+        '&:hover': {
+          backgroundColor: '#2E7112',
+        },
+      }}>
+        Register
+      </Button>
+
       <Button variant="contained"
         color="primary" 
         type="submit" 
@@ -108,7 +148,6 @@ const LogForm = () => {
       }}>
         Forgot my password
       </Button>
-
 
       
 
